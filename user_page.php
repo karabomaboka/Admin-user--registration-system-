741284ff9cfa18dd828092eb4661b130bd35aca9
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2023-01-17 14:13:25 +0200

    Update user_page.php

--- user_page.php
+++ user_page.php
@@ -28,7 +28,7 @@ if(!isset($_SESSION['user_name'])){
             <p>this is a user page</p>
             <a href="login_form.php" class="btn">login</a>
             <a href="register_form.php" class="btn">register</a>
-            <a href="logout.php" class="btn">logout</a>
+            <a href="logoutt.php" class="btn">logout</a>
         </div>
 
 
